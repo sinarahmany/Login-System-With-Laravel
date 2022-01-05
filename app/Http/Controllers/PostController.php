38752b2191d829befddeed6815709865e9cd1020
sinarahmany <sinarahmannejad@gmail.com>
2022-01-04 23:04:35 -0800

    Logged in users can view and add new posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::get(); // this is a laravel collection
+        return view('posts.index',[
+            'posts' => $posts
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body'=> 'required'
+        ]);
+
+//        $request->user()->posts()->create([
+//            'body'=>$request->body
+//        ]);
+        $request->user()->posts()->create($request->only('body'));
+
+        return back();
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <ul class="flex items-center">
             <li><a href="/" class="p-3">Home</a></li>
             <li><a href="{{route('dashboard')}}" class="p-3">Dashboard</a></li>
-            <li><a href="" class="p-3">Post</a></li>
+            <li><a href="{{route('posts')}}" class="p-3">Posts</a></li>
         </ul>
         <ul class="flex items-center">
 {{--            @if (auth()->user())--}}
